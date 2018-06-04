83134ba8bbf373cbc0b67a2bb621137be3590952
Rogerio Orioli <crorioli@hotmail.com>
2018-06-04 18:19:08 -0300

    add logout

--- js/app.js
+++ js/app.js
@@ -1,122 +1,38 @@
-// Initialize Firebase
-const config = {
-    apiKey: "AIzaSyDhJDppM9yN4I1lwTiFWQhUY0Y5cFIiZZ0",
-    authDomain: "blog-b9868.firebaseapp.com",
-    databaseURL: "https://blog-b9868.firebaseio.com",
-    projectId: "blog-b9868",
-    storageBucket: "blog-b9868.appspot.com",
-    messagingSenderId: "523133583921"
-  };
-  firebase.initializeApp(config);
-  
-  const //send leads
-        name = document.querySelector('#name'),
-        email = document.querySelector('#email'),
-        phone = document.querySelector('#phone'),
-        alert = document.querySelector('.alert'),
-        button = document.querySelector('.send-form')
-
- const // login 
-        overlay = document.querySelector('.overlay'),  
-        userEmail = document.querySelector('#email-user'),
-        password = document.querySelector('#password'),
-        alertLogin = document.querySelector('.alert-login'),
-        btnLogin = document.querySelector('.btn-login'),
-        logout = document.querySelector('.logout')
-
-
-  // start db reference  connection
-  const dbRefer = firebase.database().ref('leads')
-
-
-  // class reference 
-  class FormLeads {
-      constructor() {
-            this.name =  name.value,  
-            this.email = email.value,
-            this.phone = phone.value,
-            this.date = new Date().toISOString().replace('-', '/').split('T')[0].replace('-', '/'),
-            this.time = new Date().getTime()
-          }
-  }
-
-  // send form
-  const sendForm = () => {
-    dbRefer.push(new FormLeads())
-  
-  }
-//CLEAN FORM
-    const cleanForm =  () => {
-          name.value = '' 
-          email.value = ''
-          phone.value = ''
-    }
-
-  //validate inputs   
-
-  const validateForm = () => { 
-    if(name.value   === '' || email.value === '' || phone.value === '') {
-        alert.classList.add('alert-danger','display')
-        alert.innerHTML = 'one or more ipunts have wrong value , try agin  &#x1F915; '
-        cleanForm()
-    }else {
-        alert.classList.remove('alert-danger')
-        alert.classList.add('alert-success','display')
-        alert.innerHTML = 'gotcha you are a new lead &#x1f604'
-        sendForm() // send form function
-        cleanForm()
-    }
+label {
+    margin: 10px 0;
+}
+.btn {
+    margin-top: 20px;
+    float: right;
+}
+.alert {
+    display: none;
+}
+.alert-login {
+    display: block;
+}
+.logout{
+    display: none;
+}
+.display {
+    display: block;
+}
+.overlay{
+    background: rgba(0,0,0,.8);
+    height: 100%;
+    position: fixed;
+    width: 100%;
+    z-index: 9999999;
+    top: 0;
+    display: none;
 }
 
-// submit form 
-button.addEventListener('click', () => {
-    validateForm();
-})
 
-//login validade
-firebase.auth().onAuthStateChanged(user => {
-    if (user) { // user logged
-        overlay.style.display = 'none';
-        logout.style.display = 'block';
-        
-        
-    } else { // user not logged
-        overlay.style.display = 'block';
-        alertLogin.classList.add('alert', 'alert-danger')
-        alertLogin.innerHTML = 'You need be a logged'
-
-    }
-})
-
-//login function 
-const loginFunction = (Email, Pass) => {
-    Email = userEmail.value
-    Pass = password.value
-    //console.log(Email, Pass)
-    firebase.auth().signInWithEmailAndPassword(Email, Pass).catch(function(error) {
-
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-    
-        alertLogin.classList.add('alert-danger')
-        alertLogin.innerHTML = errorCode, + errorMessage
-        // ...
-    });
-
+.modal-login {
+    max-width: 750px;
+    background: #ffff;
+    padding: 20px;
+    margin: 25vh auto;
+    display: flow-root;
+    border-radius: 20px;
 }
-
-const logoutFunction = () => {
-    firebase.auth().signOut()
-    .then(function(success) {
-         success =  alertLogin.innerHTML = 'You need be make another login'
-    })
-    .catch(function(error) {
-        logout.style.display = 'none';
-    });
-}
-
-// login event
-btnLogin.addEventListener('click', () => {
-    loginFunction();
-})
